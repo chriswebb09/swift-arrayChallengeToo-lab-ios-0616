bd654954814776b8e37c95547b4eeec689709893
chriswebb09 <chris.webb5249@gmail.com>
2016-07-12 14:40:21 -0400

    Done

--- ArrayChallengeDeuce2/ViewController.swift
+++ ArrayChallengeDeuce2/ViewController.swift
@@ -19,7 +19,25 @@ class ViewController: UIViewController {
 
    
     
-    
+    func addNameToDeliLine(name: String) -> String {
+        var greeting: String = ""
+        if (name == "Billy Crystal") || (name == "Meg Ryan") {
+            deliLine.insert(name, atIndex: 0)
+            if (name == "Billy Crystal") {
+                greeting = "Welcome Billy! You can sit wherever you like."
+            } else if (name == "Meg Ryan") {
+                greeting = "Welcome Meg! You can sit wherever you like."
+            }
+        } else {
+            deliLine.append(name)
+            if (deliLine.count == 1) {
+                greeting = "Welcome \(name), you're first in line!"
+            } else {
+                greeting = "Welcome \(name), you're number \(deliLine.count) in line."
+            }
+        }
+        return greeting
+    }
     // Create your methods here
 
 
